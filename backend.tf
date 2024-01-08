718786adfabf5a2f9126a16db8a7794ba2ab2a23
mouni4598 <96536542+mouni4598@users.noreply.github.com>
2024-01-08 10:17:28 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-s3-bucket-for-tf-state"
+    bucket = "alpha-s3-bucket-for-tf"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table-tf"
+    dynamodb_table = "alpha-dynamo-db-table-tf"
   }
 }
